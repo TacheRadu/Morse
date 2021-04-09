e8508215700d76836458572974422c49034d849d
Iulian Peiu <iulianpeiu6@gmail.com>
2021-04-09 20:45:43 +0300

    Add messenger implementations

---
+++ app/src/main/java/com/messenger/MessengerContact.java
@@ -0,0 +1,21 @@
+package com.messenger;
+
+import com.morse.Contact;
+import com.morse.Message;
+
+public class MessengerContact implements Contact {
+    @Override
+    public void getMessages(int messageNumber) {
+
+    }
+
+    @Override
+    public void sendMessage(Message message) {
+
+    }
+
+    @Override
+    public void refreshMessageList() {
+
+    }
+}
---
+++ app/src/main/java/com/messenger/MessengerMessage.java
@@ -0,0 +1,13 @@
+package com.messenger;
+
+import com.morse.Message;
+
+public class MessengerMessage implements Message {
+
+    private String messageText;
+
+    @Override
+    public void send() {
+
+    }
+}
