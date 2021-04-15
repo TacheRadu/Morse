cd0f8e0c6fcbba3360c4bf94a6559183d9bee37e
Hristea Ionut <hristeaionut72@gmail.com>
2021-04-15 23:03:21 +0300

    add something

--- app/src/main/java/com/androidsms/SmsContact.java
+++ app/src/main/java/com/androidsms/SmsContact.java
@@ -1,9 +1,67 @@
 package com.androidsms;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.Loader;
+
 import com.morse.Contact;
 import com.morse.Message;
+import com.morse.R;
 
-public class SmsContact implements Contact {
+public class SmsContact extends Fragment implements Contact, LoaderManager.LoaderCallbacks<Cursor>,
+        AdapterView.OnItemClickListener {
+
+    @SuppressLint("InlinedApi")
+    private final static String[] FROM_COLUMNS = {
+            ContactsContract.Contacts.DISPLAY_NAME_PRIMARY
+    };
+
+    private final static int[] TO_IDS = {
+            android.R.id.text1
+    };
+
+    ListView contactsList;
+    long contactId;
+    String contactKey;
+    Uri contactUri;
+    private SimpleCursorAdapter cursorAdapter;
+
+    public SmsContact() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.contact_list_fragment,
+                container, false);
+    }
+
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        contactsList = (ListView) getActivity().findViewById(android.R.id.list);
+        cursorAdapter = new SimpleCursorAdapter(
+                getActivity(),
+                R.layout.contacts_list_item,
+                null,
+                FROM_COLUMNS, TO_IDS,
+                0);
+        //contactsList.setAdapter(cursorAdapter);
+    }
+
     @Override
     public void getMessages(int messageNumber) {
 
@@ -16,6 +74,27 @@ public class SmsContact implements Contact {
 
     @Override
     public void refreshMessageList() {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
 
     }
 }
