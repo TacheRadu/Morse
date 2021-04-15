ced7c803d08baf0babb5e087f5bafea35e98cc60
Neculau Tudor <neculautudor02@gmail.com>
2021-04-15 13:14:09 +0300

    created the small send method. needs further external coding to work

--- app/src/main/java/com/androidsms/SmsMessage.java
+++ app/src/main/java/com/androidsms/SmsMessage.java
@@ -1,13 +1,29 @@
 package com.androidsms;
 
+import android.telephony.SmsManager;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.morse.Message;
 
-public class SmsMessage implements Message {
+public class SmsMessage extends AppCompatActivity implements Message {
 
     private String messageText;
 
     @Override
     public void send() {
 
+    }
+    public void send(String message, String phNumber){
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phNumber, null, message, null, null);
+            Toast.makeText(this, "Message sent", Toast.LENGTH_SHORT).show();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            Toast.makeText(this, "Message failed", Toast.LENGTH_SHORT).show();
+        }
     }
 }
