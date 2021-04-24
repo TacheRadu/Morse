c379f85be6d4e56087cf68d4653750975f1432a7
Iulian Peiu <iulianpeiu6@gmail.com>
2021-04-24 20:19:18 +0300

    Fix person is null error

--- app/src/main/java/com/androidsms/SmsContact.java
+++ app/src/main/java/com/androidsms/SmsContact.java
@@ -1,8 +1,11 @@
 package com.androidsms;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
+import android.provider.ContactsContract;
+
 import androidx.appcompat.app.AppCompatActivity;
 import com.morse.Contact;
 import java.util.ArrayList;
@@ -50,7 +53,8 @@ public class SmsContact extends AppCompatActivity implements Contact {
 
                 if (currentAddress.equals(fromAddress)) {
                     MessageInfo currentMessage = new MessageInfo(cursor.getString(cursor.getColumnIndexOrThrow("person")),
-                            cursor.getString(cursor.getColumnIndexOrThrow("address")),
+                            getContactName(context,
+                                    cursor.getString(cursor.getColumnIndexOrThrow("address"))),
                             cursor.getString(cursor.getColumnIndexOrThrow("body")),
                             cursor.getString(cursor.getColumnIndexOrThrow("date")),
                             cursor.getString(cursor.getColumnIndexOrThrow("seen")));
@@ -62,6 +66,26 @@ public class SmsContact extends AppCompatActivity implements Contact {
         }
 
         return messages;
+    }
+
+    private String getContactName(Context context, String phoneNumber) {
+        ContentResolver cr = context.getContentResolver();
+        Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI,
+                Uri.encode(phoneNumber));
+        Cursor cursor = cr.query(uri,
+                new String[] { ContactsContract.PhoneLookup.DISPLAY_NAME }, null, null, null);
+        if (cursor == null) {
+            return null;
+        }
+        String contactName = null;
+        if (cursor.moveToFirst()) {
+            contactName = cursor.getString(cursor
+                    .getColumnIndex(ContactsContract.PhoneLookup.DISPLAY_NAME));
+        }
+        if (cursor != null && !cursor.isClosed()) {
+            cursor.close();
+        }
+        return contactName;
     }
 
 }
