2541d6b48cc3d670c962cccf573cc1ccd6cf2e4a
Hristea Ionut <hristeaionut72@gmail.com>
2021-05-09 17:01:14 +0300

    add twitter login

---
+++ app/src/main/java/com/channels/twitter/HomeActivity.java
@@ -0,0 +1,23 @@
+package com.channels.twitter;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.R;
+
+public class HomeActivity extends AppCompatActivity {
+    TextView name;
+    String user;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_twitter_home);
+
+        user=getIntent().getStringExtra("username");
+        name=(TextView)findViewById(R.id.nametextView);
+        name.setText(user);
+
+    }
+}
--- app/src/main/java/com/channels/twitter/TwitterChannelLoginActivity.java
+++ app/src/main/java/com/channels/twitter/TwitterChannelLoginActivity.java
@@ -14,7 +14,18 @@ import com.morse.Channel;
 import com.morse.Contact;
 import com.morse.Message;
 
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthToken;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
 import java.util.List;
+
+import twitter4j.TwitterFactory;
 
 
 /**
@@ -23,16 +34,16 @@ import java.util.List;
  *
  * @author Ionuț Roșca
  * @author Ionuț Hristea
- * @version 0.1.1
+ * @version 0.2.0
  */
 public class TwitterChannelLoginActivity extends AppCompatActivity implements Channel {
     private AppCompatActivity parentActivity = this;
     private EditText userCredential;
     private EditText userPassword;
-    private Button loginButton;
     private TextView remainingAttempts;
     private boolean credentialsCheckPassed = false;
     private int currentNumberOfAvailableRetries = 3;
+    TwitterLoginButton loginButton;
 
     public TwitterChannelLoginActivity() {
     }
@@ -44,53 +55,41 @@ public class TwitterChannelLoginActivity extends AppCompatActivity implements Ch
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Twitter.initialize(this);
         setContentView(R.layout.twitter_login_activity);
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                TwitterSession session = TwitterCore.getInstance().getSessionManager().getActiveSession();
+                TwitterAuthToken authToken = session.getAuthToken();
 
-        this.userCredential = findViewById(R.id.userCredential);
-        this.userPassword = findViewById(R.id.userPassword);
-        this.loginButton = findViewById(R.id.loginButton);
-        this.remainingAttempts = findViewById(R.id.remainingAttemps);
 
-        loginButton.setOnClickListener(v -> {
-            String inputName = userCredential.getText().toString();
-            String inputPassword = userPassword.getText().toString();
+                loginMethod(session);
+            }
 
-            if (inputName.isEmpty() || inputPassword.isEmpty()) {
-                Toast.makeText(TwitterChannelLoginActivity.this,
-                        "You didn't forgot anything? :)", Toast.LENGTH_SHORT).show();
-            } else {
-                credentialsCheckPassed = validateCredentials(inputName, inputPassword);
-                if (credentialsCheckPassed) {
-                    Toast.makeText(TwitterChannelLoginActivity.this,
-                            "Login successful", Toast.LENGTH_SHORT).show();
-
-                    /* If we are here it means that the user successfully logged in, thus
-                     * move to the next activity and that should be the activity the user sees
-                     * after successfully logging in.
-                     */
-                    Intent intent = new Intent(TwitterChannelLoginActivity.this,
-                            TwitterChannelLoginActivity.class);
-                    startActivity(intent);
-                } else {
-                    currentNumberOfAvailableRetries--;
-                    Toast.makeText(TwitterChannelLoginActivity.this,
-                            "Incorrect username or password", Toast.LENGTH_SHORT).show();
-
-                    remainingAttempts.setText(currentNumberOfAvailableRetries);
-                    if (currentNumberOfAvailableRetries == 0) {
-                        loginButton.setEnabled(false);
-                    }
-                }
+            @Override
+            public void failure(TwitterException exception) {
+                Toast.makeText(getApplicationContext(),"Login fail",Toast.LENGTH_LONG).show();
             }
         });
     }
 
-    private boolean validateCredentials(String userCredential, String userPassword) {
-        /* TODO: Implement this to use the Twitter's API in order to check the validity of the
-         * credentials.
-         */
-        return true;
+    public void loginMethod(TwitterSession twitterSession){
+        System.out.println("test");
+        String userName=twitterSession.getUserName();
+        Intent intent= new Intent(TwitterChannelLoginActivity.this, HomeActivity.class);
+        intent.putExtra("username",userName);
+        startActivity(intent);
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        System.err.println("session");
+        super.onActivityResult(requestCode, resultCode, data);
+
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
 
     @Override
     public Intent getIntent() {
